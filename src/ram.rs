2d56dcfb4e92fa3de7f1f79a2ca2bcf763fbedcb
Blikq <blikqmain@gmail.com>
2023-12-07 11:54:36 +0100

    added ram

---
+++ src/ram.rs
@@ -0,0 +1,19 @@
+struct Ram{
+    memory: [u8; 4096]
+}
+
+impl Ram{
+    fn new() -> Self{
+        let mut ram = Ram{memory: [0; 4096]};
+
+        ram
+    }
+
+    pub fn write(&mut self, address: u16, value: u8) {
+        self.memory[address as usize] = value;
+    }
+
+    pub fn read(&self, address: u16) -> u8 {
+        self.memory[address as usize]
+    }
+}
